d6d26a01c331ac871b14d22c78663f487803da45
totom3ndez <tomii.mendez45@gmail.com>
2022-12-16 13:08:49 +0100

    updaste

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
 	return (
 		<div className=' text-white h-screen bg-gradient-to-r from-[#e0c3fc] to-[#8ec5fc]'>
 			<header className='flex bg-gray-800 text-white px-28 py-5 items-center justify-between'>
-				<Link href={'/'}>
+				<Link href={'/'} as={process.env.BACKEND_URL + '/'}>
 					<h1 className='font-black text-lg'>
 						Products
 						<span className='ml-2 text-gray-400 font-bold'>
@@ -19,7 +19,11 @@ const Layout = ({ children }) => {
 				</Link>
 
 				<div>
-					<Link className='text-right' href={'/new'}>
+					<Link
+						className='text-right'
+						href={'/new'}
+						as={process.env.BACKEND_URL + '/new'}
+					>
 						<button className='bg-green-500 inline-flex items-center hover:bg-green-400 px-5 py-2 font-bold rounded'>
 							<AiOutlinePlusCircle className='mr-2' />
 							Add Product
